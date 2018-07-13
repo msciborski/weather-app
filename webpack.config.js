2e497f1acec7e09fa1bf86168b08fa00be5996fc
msciborski <michal.sciborski@student.put.poznan.pl>
2018-07-13 23:06:09 +0200

    Git init, react boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const absolutePath = path.join(__dirname, 'public');
+const absolutePathToSrc = path.join(__dirname, 'src');
+module.exports = {
+  entry: {
+    app: './src/app.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: absolutePath,
+  },
+  resolve: {
+    modules: ['node_modules', absolutePathToSrc],
+    extensions: ['*', '.js', '.jsx'],
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: absolutePath,
+  },
+  module: {
+    rules: [{
+      test: /(\.js|\.jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader'],
+    },
+    {
+      test: /(\.css|\.scss|\.sass)$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    }],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['public']),
+    new HtmlWebpackPlugin(
+      {
+        template: 'index.html',
+      },
+    ),
+  ],
+};
