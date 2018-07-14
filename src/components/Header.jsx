6f51899a39dd0feca2f71c6b47b6a08b3c8b554a
msciborski <michal.sciborski@student.put.poznan.pl>
2018-07-14 22:32:34 +0200

    Deleting row and columns, Deleting purecss, creating header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = (props) => {
+  const { title } = props;
+
+  return (
+    <div className="header">
+      <h1>
+        { title }
+      </h1>
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Weather App',
+};
+Header.propTypes = {
+  title: PropTypes.string,
+};
+export default Header;
