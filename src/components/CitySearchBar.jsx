64a01edd1c005db7ad308acd814ba327cb8fc010
msciborski <michal.sciborski@student.put.poznan.pl>
2018-07-19 20:46:45 +0200

    Error handling on citysearchbar

--- src/components/CitySearchBar.jsx
+++ src/components/CitySearchBar.jsx
@@ -3,28 +3,22 @@ import PropTypes from 'prop-types';
 import InputWithPlaceholder from './InputWithPlaceholder';
 
 export default class CitySearchBar extends Component {
-  state = {
-    error: undefined,
-  };
-
   handleCityChange = (e) => {
     const { setCity } = this.props;
     const city = e.target.value.trim();
     const error = setCity(city);
-
     this.setState(() => ({ error }));
   }
 
   render() {
-    const { error } = this.state;
-    const inputStyle = error ? 'search-bar__form__input-field error'
-      : 'search-bar__form__input-field';
-    const placeHolder = 'City';
+    const { fetchError } = this.props;
+    const inputStyle = !fetchError ? 'search-bar__form__input-field'
+      : 'search-bar__form__input-field error';
     return (
       <div className="search-bar">
         <form id="form-city" name="form-city" className="search-bar__form">
           <InputWithPlaceholder
-            placeHolder={placeHolder}
+            placeHolder="City"
             inputStyle={inputStyle}
             onChangeHandler={this.handleCityChange}
           />
@@ -36,4 +30,5 @@ export default class CitySearchBar extends Component {
 
 CitySearchBar.propTypes = {
   setCity: PropTypes.func.isRequired,
+  fetchError: PropTypes.string.isRequired,
 };
--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -3,32 +3,63 @@ import _ from 'lodash';
 import Grid from './Grid';
 import Header from './Header';
 import WeatherContent from './WeatherContent';
+import CitySearchBar from './CitySearchBar';
+
+const API_KEY = '9751d95f8393e1ba8fe312a569747b91';
 
 export default class WeatherApp extends Component {
   state = {
     city: undefined,
+    weather: undefined,
+    units: 'metric',
+    error: undefined,
+    lang: 'pl',
   }
 
   componentDidUpdate(prevProps, prevState) {
     const { city } = this.state;
+    const { units } = this.state;
+    const { lang } = this.state;
     if (prevState.city !== city) {
-      console.log(city);
+      this.fetchWeather(city, units, lang);
     }
   }
 
   setCity = _.debounce((city) => {
     this.setState(() => ({ city }));
-  }, 1000);
+  }, 400);
+
+  setError = (error) => {
+    this.setState(() => ({ error }));
+  }
+
+  setWeather = (weather) => {
+    this.setState(() => ({ weather }));
+    this.setError(undefined);
+  }
+
+  fetchWeather = (city, units, lang) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=${units}&lang=${lang}`;
+    fetch(url)
+      .then(response => response.json())
+      .then((responseJson) => {
+        const { cod } = responseJson;
+        if (cod === '200') {
+          this.setWeather(responseJson);
+        } else {
+          this.setError(responseJson.message);
+        }
+      });
+  }
 
   render() {
-    const { city } = this.state;
+    const { weather } = this.state;
+    const { error } = this.state;
     return (
       <Grid>
         <Header />
-        <WeatherContent setCity={this.setCity} />
-        <h1>
-          {city}
-        </h1>
+        <CitySearchBar setCity={this.setCity} fetchError={error} />
+        {weather && <WeatherContent weather={weather} />}
       </Grid>
     );
   }
