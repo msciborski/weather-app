3f89837f20f30616b4358432914fad05f7a1ba62
msciborski <michal.sciborski@student.put.poznan.pl>
2018-07-23 22:47:53 +0200

    Fix bug with selection, handle click outside the component

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -18,14 +18,50 @@ export default class Dropdown extends Component {
     };
   }
 
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleMouseClickedOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleMouseClickedOutside);
+  }
+
   setDropdownRef = (node) => {
     this.node = node;
   }
 
   setSelectedItem = (element) => {
-    const selectedBeforeElement = document.querySelector('[selected]');
-    this.removeSelectionFromItem(selectedBeforeElement);
+    const selectedBeforeElement = this.getSelectedElement();
+    if (selectedBeforeElement) {
+      this.removeSelectionFromItem(selectedBeforeElement);
+    }
     element.setAttribute('selected', '');
+  }
+
+  getElementList = () => {
+    if (this.node) {
+      return _.filter(this.node.childNodes, node => node.id === 'drop-down__list')[0];
+    }
+    return undefined;
+  }
+
+  getSelectedElement = () => {
+    const elementList = this.getElementList();
+    if (elementList) {
+      const elements = elementList.childNodes;
+      if (elements.length > 0) {
+        const selectedElement = _.filter(elements, element => element.hasAttribute('selected'));
+        return selectedElement[0];
+      }
+    }
+    return undefined;
+  }
+
+  handleMouseClickedOutside = (e) => {
+    if (this.node && !this.node.contains(e.target)) {
+      const elementList = this.getElementList();
+      elementList.classList.remove('opened');
+    }
   }
 
   removeSelectionFromItem = (selectedItem) => {
@@ -41,6 +77,11 @@ export default class Dropdown extends Component {
     this.setSelectedItem(e.target);
   }
 
+  handleDropdownClick = () => {
+    const elementList = this.getElementList();
+    elementList.classList.toggle('opened');
+  }
+
   render() {
     const { dropDownId } = this.state;
     const { optionsList } = this.state;
@@ -51,6 +92,7 @@ export default class Dropdown extends Component {
       <div
         ref={this.setDropdownRef}
         role="button"
+        onClick={this.handleDropdownClick}
         tabIndex="0"
         id={dropDownId}
         className={`drop-down ${dropDownStyle}`}
@@ -58,7 +100,6 @@ export default class Dropdown extends Component {
         <span className="drop-down__label">
           {title}
         </span>
-        <span className="drop-down__bar" />
         <ul id="drop-down__list">
           {optionsList}
         </ul>
