d60e5fd93aea8d2f64c790bdbca9894308b8c566
msciborski <michal.sciborski@student.put.poznan.pl>
2018-08-01 21:37:11 +0200

    Creating forecast utils and mock

---
+++ src/utilites/ForecastUtils.js
@@ -0,0 +1,43 @@
+export default class ForecastUtils {
+  static GetForecast(weatherForeacast) {
+    const MapForecastToShortForecast = (forecast) => {
+      const { all } = forecast.clouds;
+      const { dt } = forecast;
+
+      const {
+        temp,
+        temp_min,
+        temp_max,
+        pressure,
+        sea_level,
+      } = forecast.main;
+
+      const { description, main } = forecast.weather[0];
+      return {
+        dateTimeStamp: dt,
+        temp,
+        tempMin: temp_min,
+        tempMax: temp_max,
+        pressure,
+        seaLevel: sea_level,
+        description,
+        main,
+        clouds: all,
+      };
+    };
+
+    const { list } = weatherForeacast;
+    const forecasts = [];
+    let i = 0;
+    while (i < list.length) {
+      if (i >= 12) {
+        break;
+      }
+      const shortForecast = MapForecastToShortForecast(list[i]);
+      forecasts.push(shortForecast);
+      i += 1;
+    }
+    return forecasts;
+  }
+
+}
