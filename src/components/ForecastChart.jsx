7b541c7a2a7aa229094fbcd3587361c5ecf92199
msciborski <michal.sciborski@student.put.poznan.pl>
2018-08-04 19:49:45 +0200

    Changing position of tooltip, basic styling for tooltip

--- src/components/ForecastChart.jsx
+++ src/components/ForecastChart.jsx
@@ -54,13 +54,6 @@ export default class ForecastChart extends Component {
         display: true,
         text: 'Poznan',
       },
-      // scales: {
-      //   xAxes: [{
-      //     ticks: {
-      //       display: false,
-      //     },
-      //   }],
-      // },
       tooltips: {
         enabled: false,
         position: 'nearest',
@@ -71,58 +64,65 @@ export default class ForecastChart extends Component {
   });
 
   customTooltip = (tooltipModel) => {
-    console.log(tooltipModel);
-    if (tooltipModel) {
-      let tooltipEl = document.getElementById('chartjs-tooltip');
-      // Create tooltip element, when tooltip dosen't exist
-      if (!tooltipEl) {
-        tooltipEl = document.createElement('div');
-        tooltipEl.id = 'chartjs-tooltip';
-        tooltipEl.classList.add('weather-content__header__forecast-chart__tooltip');
-        tooltipEl.innerHTML = '<div class="weather-content__header__forecast-chart__tooltip__body"></div>';
-        document.body.appendChild(tooltipEl);
-      }
+    let tooltipEl = document.getElementById('chartjs-tooltip');
+    // Create tooltip element, when tooltip dosen't exist
+    if (!tooltipEl) {
+      tooltipEl = document.createElement('div');
+      tooltipEl.id = 'chartjs-tooltip';
+      tooltipEl.classList.add('weather-content__header__forecast-chart__tooltip');
+      tooltipEl.innerHTML = '<div class="weather-content__header__forecast-chart__tooltip__body"></div>';
+      document.body.appendChild(tooltipEl);
+    }
 
-      // Hide if tooltip opacity is set 0
-      if (tooltipModel.opacity === 0) {
-        tooltipEl.style.opacity = 0;
-        return;
-      }
+    // Hide if tooltip opacity is set 0
+    if (tooltipModel.opacity === 0) {
+      tooltipEl.style.opacity = 0;
+    }
 
-      // Set carret position
-      tooltipEl.classList.remove('above', 'below', 'no-transform');
-      if (tooltipModel.yAlign) {
-        tooltipEl.classList.add(tooltipModel.yAlign);
-      } else {
-        tooltipEl.classList.add('no-transform');
-      }
+    // Set carret position
+    tooltipEl.classList.remove('above', 'below', 'no-transform');
+    if (tooltipModel.yAlign) {
+      tooltipEl.classList.add(tooltipModel.yAlign);
+    } else {
+      tooltipEl.classList.add('no-transform');
+    }
 
-      if (tooltipModel.body) {
-        const label = tooltipModel.dataPoints[0].xLabel;
-        const { index } = tooltipModel.dataPoints[0];
-        const { tempMin } = this.state;
-        const { tempMax } = this.state;
-        const { pressure } = this.state;
-        const { description } = this.state;
+    if (tooltipModel.body) {
+      const label = tooltipModel.dataPoints[0].xLabel;
+      const { index } = tooltipModel.dataPoints[0];
+      const { tempMin } = this.state;
+      const { tempMax } = this.state;
+      const { pressure } = this.state;
+      const { description } = this.state;
 
-        const labelHtml = `<div class="weather-content__header__forecast-chart__tooltip__body__label"><span>${label}</span></div>`;
-        const tempMinHtml = `<div class="weather-content__header__forecast-chart__tooltip__body__info"><span>Temp min:</span><span>${tempMin[index]}</span></div>`;
-        const tempMaxHtml = `<div class="weather-content__header__forecast-chart__tooltip__body__info"><span>Temp max:</span><span>${tempMax[index]}</span></div>`;
-        const pressureHtml = `<div class="weather-content__header__forecast-chart__tooltip__body__info"><span>Pressure:</span><span>${pressure[index]}</span></div>`;
+      const labelHtml = `<div class="weather-content__header__forecast-chart__tooltip__body__label">
+                                  <span>${label}</span>
+                           </div>`;
+      const tempMinHtml = `<div class="weather-content__header__forecast-chart__tooltip__body__info">
+                                  <span class="weather-content__header__forecast-chart__tooltip__body__info__title">Temp min</span>
+                                  <span>${tempMin[index]}</span>
+                            </div>`;
+      const tempMaxHtml = `<div class="weather-content__header__forecast-chart__tooltip__body__info">
+                                  <span class="weather-content__header__forecast-chart__tooltip__body__info__title">Temp max</span>
+                                  <span>${tempMax[index]}</span>
+                            </div>`;
+      const pressureHtml = `<div class="weather-content__header__forecast-chart__tooltip__body__info">
+                                  <span class="weather-content__header__forecast-chart__tooltip__body__info__title">Pressure</span>
+                                  <span>${pressure[index]}</span>
+                              </div>`;
 
-        const rootTooltip = tooltipEl.querySelector('div');
-        rootTooltip.innerHTML = `${labelHtml}${tempMinHtml}${tempMaxHtml}${pressureHtml}`;
+      const rootTooltip = tooltipEl.querySelector('div');
+      rootTooltip.innerHTML = `${labelHtml}${tempMinHtml}${tempMaxHtml}${pressureHtml}`;
 
-        const { canvas } = this.chart;
-        const position = canvas.getBoundingClientRect();
-        // CSS that have to be set in js, becouse we need to know position of caret and canvas
+      const { canvas } = this.chart;
+      const position = canvas.getBoundingClientRect();
+      // CSS that have to be set in js, becouse we need to know position of caret and canvas
 
-        tooltipEl.style.opacity = 1;
-        tooltipEl.style.position = 'absolute';
-        tooltipEl.style.left = `${position.left + tooltipModel.caretX}px`;
-        tooltipEl.style.top = `${position.top + tooltipModel.caretY + 50}px`;
-        tooltipEl.style.padding = `${tooltipModel.yPadding}px ${tooltipModel.xPadding}px`;
-      }
+      tooltipEl.style.opacity = 1;
+      tooltipEl.style.position = 'absolute';
+      tooltipEl.style.left = `${position.left + tooltipModel.caretX}px`;
+      tooltipEl.style.top = `${position.top}px`;
+      tooltipEl.style.padding = `${tooltipModel.yPadding}px ${tooltipModel.xPadding}px`;
     }
   }
 
